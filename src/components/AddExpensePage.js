b42711ec523f6db3ccc61d5668db53b01a8e9be1
Tiago Soriano <tiago.ninjarj@gmail.com>
2020-05-22 19:20:35 -0400

    Add new features to the AddExpensePage such as date picker

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import ExpenseForm from "./ExpenseForm";
+import { useDispatch } from "react-redux";
+import { addExpense } from "../actions/expenses";
 
 export default function AddExpensePage() {
+	const dispatch = useDispatch();
+	const history = useHistory();
+
 	return (
 		<div>
 			<h1>Add Expense</h1>
-			<ExpenseForm />
+			<ExpenseForm
+				onSubmit={expense => {
+					dispatch(addExpense(expense));
+					history.push("/");
+				}}
+			/>
 		</div>
 	);
 }
